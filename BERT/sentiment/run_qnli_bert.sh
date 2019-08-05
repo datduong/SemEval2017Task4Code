f1c0d8df86a83913ae242e0feca653d1ddfea039
Dat Duong <datd1988@gmail.com>
2019-08-05 15:38:13 -0700

    Update run_qnli_bert.sh

--- BERT/sentiment/run_qnli_bert.sh
+++ BERT/sentiment/run_qnli_bert.sh
@@ -30,4 +30,3 @@ tokenizer_name='bert-base-cased'
 
 CUDA_VISIBLE_DEVICES=4 python3 -u run_glue.py --data_dir $data_dir --model_type bert --model_name_or_path $model_name_or_path --task_name qnli --output_dir $output_dir --config_name $config_name --tokenizer_name $tokenizer_name --num_train_epochs 10 --do_eval --max_seq_length 512 --overwrite_output_dir > $output_dir/track.log
 
-do_eval
