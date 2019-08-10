636b8c3aeff3d38daa85ef4ec125a320ad0d67cf
Dat Duong <datd1988@gmail.com>
2019-08-09 22:44:38 -0700

    change sh

--- BERT/sentiment/run_qnli_bert.sh
+++ BERT/sentiment/run_qnli_bert.sh
@@ -38,7 +38,6 @@ model_name_or_path='/local/datdb/SemEval2017Task4/4B-English/BertFineTune/' ## l
 config_name=$model_name_or_path/'bert_config.json'
 tokenizer_name='bert-base-cased'
 
-
 CUDA_VISIBLE_DEVICES=4 python3 -u run_glue.py --data_dir $data_dir --model_type bert --model_name_or_path $model_name_or_path --task_name qnli --output_dir $output_dir --config_name $config_name --tokenizer_name $tokenizer_name --num_train_epochs 10 --do_train --max_seq_length 512 --overwrite_output_dir --fp16 --evaluate_during_training --num_segment_type 6 > $output_dir/track.log
 
 
